e143d844554d301cceb6afc053b5e398f5b097b4
巴为焱 <1160053654@qq.com>
2016-05-09 10:04:39 +0800

    修改的内容再次提交

--- GitDemoOne/app/src/main/java/com/ba/dllo/gitdemoone/MainActivity.java
+++ GitDemoOne/app/src/main/java/com/ba/dllo/gitdemoone/MainActivity.java
@@ -3,6 +3,9 @@ package com.ba.dllo.gitdemoone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * hulalall
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //修改内容
+
 }
